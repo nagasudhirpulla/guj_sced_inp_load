12593a3a715227d24d86a6389032b46bbf359c5f
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-09-26 19:55:13 +0530

    minor changes

--- run_data_pipeline.bat
+++ run_data_pipeline.bat
@@ -1,4 +1,4 @@
 call .\activate_env.bat
-call python index_load_from_folder.py --date 2021-09-17
-call python index_run_gams_from_excel.py --date 2021-09-17
-call python index_publish_results_to_excel.py --date 2021-09-17
+call python index_load_from_folder.py
+call python index_run_gams_from_excel.py
+call python index_publish_results_to_excel.py
