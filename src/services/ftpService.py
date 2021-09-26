b1ad3010a8e17bfb435f4de34919d62f1defdc26
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-09-26 10:04:59 +0530

    minor changes for ftp hanging

--- src/services/ftpService.py
+++ src/services/ftpService.py
@@ -1,19 +1,25 @@
 import ftplib
 import os
+import time
 
 
 def uploadFileToFtp(localFilePath: str, ftpHost: str, ftpUname: str, ftpPass: str, remoteWorkingDirectory: str) -> bool:
-    # TODO complete this
+    isUploadSuccess: bool = False
     _, targetFilename = os.path.split(localFilePath)
     # connect to the FTP server
     ftp = ftplib.FTP(ftpHost, ftpUname, ftpPass)
     if not (remoteWorkingDirectory == None or remoteWorkingDirectory.strip() == ""):
-        ftp.cwd(remoteWorkingDirectory)
+        _ = ftp.cwd(remoteWorkingDirectory)
+        time.sleep(1)
 
     # Read file in binary mode
     with open(localFilePath, "rb") as file:
-        ftp.storbinary(f"STOR {targetFilename}", file)
-    
+        retCode = ftp.storbinary(f"STOR {targetFilename}", file)
+        time.sleep(1)
+
     # quit and close the connection
     ftp.quit()
-    return True
+
+    if retCode.startswith('226'):
+        isUploadSuccess = True
+    return isUploadSuccess
