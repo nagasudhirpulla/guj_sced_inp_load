e38993863582bffb3e440964d6e59dfd0d022bbd
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2021-11-12 10:42:02 +0530

    real time revision bugs fix

--- run_data_pipeline.bat
+++ run_data_pipeline.bat
@@ -3,5 +3,5 @@ for /l %%A in (12, 1, 12) do (
 call python index_fetch_ftp_files.py --date=2021-11-%%A
 call python index_load_from_folder.py --date=2021-11-%%A
 call python index_run_gams_from_excel.py --date=2021-11-%%A
-@REM call python index_publish_results_to_excel.py --date=2021-11-%%A
+call python index_publish_results_to_excel.py --date=2021-11-%%A
 )
